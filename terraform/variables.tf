1dd6b90200629d2d5f09f53427bbf39c41f20fcc
Hung Le Van <hunglv.3h@gmail.com>
2026-05-30 12:05:26 +0700

    Sua khoang cach dong

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "bucket_name" {
-    description = "aws-personal-website-hunglv"
+    description = "Tên duy nhất của S3 Bucket dùng để chứa mã nguồn website tĩnh"
     type = string
   
 }
